af40c002f5a5dd9fe7ca43a6285d5faf9a6e4466
knmarcin <marcinknap94@gmail.com>
2019-01-03 19:58:05 +0100

    Test commit

---
+++ src/com/mk/grocerylist/Controller.java
@@ -0,0 +1,4 @@
+package com.mk.grocerylist;
+
+public class Controller {
+}
---
+++ src/module-info.java
@@ -0,0 +1,6 @@
+module groceryList {
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens com.mk.grocerylist;
+}

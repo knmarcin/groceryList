06279dffc2b9cd61fa059fbc418b3e1274dcb95e
knmarcin <marcinknap94@gmail.com>
2019-01-03 20:03:13 +0100

    Renaming files

--- src/com/mk/grocerylist/Main.java
+++ src/com/mk/grocerylist/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("mainwindow.fxml"));
         primaryStage.setTitle("Grocery List");
         primaryStage.setScene(new Scene(root, 320, 500));
         primaryStage.show();

c47f03ebe8b46656c2119db6821a469568865a7a
knmarcin <marcinknap94@gmail.com>
2019-01-04 20:41:25 +0100

    Można zapisywac i wczytywac poprzednia liste.

--- src/com/mk/grocerylist/Controller.java
+++ src/com/mk/grocerylist/Controller.java
@@ -1,16 +1,19 @@
 package com.mk.grocerylist;
 
 import com.mk.grocerylist.datamodel.groceryListItem;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.SelectionMode;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Controller {
+
 
     private List<groceryListItem> groceries;
     @FXML
@@ -21,6 +24,9 @@ public class Controller {
     private TextField textField;
     @FXML
     private Button deleteSelected;
+    @FXML
+    private MenuItem saveGroceryListToFile;
+    String line = null;
 
     public void initialize() {
 
@@ -42,12 +48,60 @@ public class Controller {
         groceriesList.getItems().remove(toDeleteIndex);
         groceries.remove(toDeleteIndex);
         groceriesList.getItems().setAll(groceries);
-        System.out.println(groceries);
+    }
 
+    @FXML
+    public void saveGroceryListToFile() throws FileNotFoundException {
+        PrintWriter pw = new PrintWriter(new FileOutputStream("grocery list.txt"));
+        pw.println(groceries);
+        pw.close();
+    }
 
+    @FXML
+
+    public void loadGroceriesFromFile() throws IOException {
+        groceries = FXCollections.observableArrayList();
+        Path path = Paths.get("grocery list.txt");
+        BufferedReader bufferedReader = Files.newBufferedReader(path);
+        String input;
+        int i = 0;
+
+        try {
+            while ((input = bufferedReader.readLine()) != null) {
+                input=input.replace("[","");
+                input=input.replace("]","");
+                String[] itemPieces = input.split(", ");
+
+                int j = itemPieces.length;
+                for (i=0;i<j;i=i+1 ) {
+                    groceryListItem item = new groceryListItem(itemPieces[i]);
+                    System.out.println(itemPieces[i]);
+                    groceries.add(item);
+                }
+                groceriesList.getItems().setAll(groceries);
+            }
+
+            } finally {
+            if(bufferedReader!=null){
+                bufferedReader.close();
+                }
+            }
+        }
     }
 
 
+//        try {
+//            FileReader fileReader = new FileReader("grocery list.txt");
+//            BufferedReader bufferedReader = new BufferedReader(fileReader);
+//
+//
+//            while ((line = bufferedReader.readLine())!= null ) {System.out.println(line);}
+//
+//            bufferedReader.close();
+//            groceries = line
+//        } catch (FileNotFoundException e) {
+//            System.out.println("Unable to open file");
+//        } catch (IOException e) {
+//            System.out.println("Error reading file");
+//        }
 
-
-}
--- src/com/mk/grocerylist/Main.java
+++ src/com/mk/grocerylist/Main.java
@@ -4,10 +4,17 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import com.mk.grocerylist.Controller;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+
 
 public class Main extends Application {
-
+TextField textField = new TextField();
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("mainwindow.fxml"));
@@ -15,10 +22,24 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 355, 500));
         primaryStage.setResizable(false);
         primaryStage.show();
+
+
     }
 
+
+    @Override
+    public void init() throws Exception {
+
+    }
+
+    @Override
+    public void stop() throws Exception {
+
+    }
 
     public static void main(String[] args) {
         launch(args);
+
     }
+
 }
